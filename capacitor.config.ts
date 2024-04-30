b08aa6e5c32b498926534c5c3a0ba4b9889efdf4
joshuak29 <rockjoshk29@gmail.com>
2024-04-30 11:04:10 +0200

    new project ts to js

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'vue.ionic.v1',
-  appName: 'vue-ionic-1',
-  webDir: 'dist'
-};
-
-export default config;
