3596950ae6cbcc831acc4f52db0d77217fa5538b
joshuak29 <rockjoshk29@gmail.com>
2024-05-18 23:07:28 +0200

    ArtistDetailsPage done sketch

--- src/pages/ArtistDetailsPage.vue
+++ src/pages/ArtistDetailsPage.vue
@@ -1,8 +1,8 @@
 <template>
-    <IonPage class="p-4">
-        <IonContent>
-            <div class="flex flex-row items-center px-4">
-                <IonAvatar class="w-20 h-auto">
+    <IonPage class="py-4 no-scrollbar">
+        <IonContent class="">
+            <div class="flex flex-row items-center px-4 mb-8">
+                <IonAvatar class="w-28 h-auto">
                     <img alt="Silhouette of a person's head"
                         src="https://ionicframework.com/docs/img/demos/avatar.svg" />
                 </IonAvatar>
@@ -16,9 +16,9 @@
             </div>
 
             <!-- about -->
-            <IonCard class="bg-transparent m-0">
+            <div class="bg-transparent m-0">
                 <IonCardHeader>
-                    <IonCardTitle class="text-2xl">9000 <span class="text-lg font-medium pl-4">Monthly
+                    <IonCardTitle class="text-2xl">9000 <span class="text-sm text-black/50 font-medium">Monthly
                             Listeners</span></IonCardTitle>
                 </IonCardHeader>
 
@@ -28,10 +28,10 @@
                     dicta veritatis asperiores repudiandae consequatur ipsam, quas eos laboriosam natus rerum? Aliquam
                     illo quasi assumenda.
                 </IonCardContent>
-            </IonCard>
+            </div>
 
             <!-- disco -->
-            <IonCard class="bg-transparent m-0">
+            <div class="bg-transparent m-0">
                 <IonCardHeader>
                     <IonCardTitle class="text-2xl text-center">Discography</IonCardTitle>
                 </IonCardHeader>
@@ -43,7 +43,7 @@
                         </div>
                         <IonList>
                             <IonItem class="flex flex-row h-fit my-4" v-for="i in 3" :key="i">
-                                <img  class="h-20" alt="Silhouette of a person's head"
+                                <img class="h-24" alt="Silhouette of a person's head"
                                     src="https://ionicframework.com/docs/img/demos/avatar.svg" />
 
                                 <div class="flex flex-col ml-4">
@@ -61,7 +61,7 @@
                         <IonList>
 
                             <IonItem class="flex flex-row justify-start h-fit my-4" v-for="i in 3" :key="i">
-                                <img  class="h-20" alt="Silhouette of a person's head"
+                                <img class="h-24" alt="Silhouette of a person's head"
                                     src="https://ionicframework.com/docs/img/demos/avatar.svg" />
 
                                 <div class="flex flex-col ml-4">
@@ -73,50 +73,40 @@
                     </div>
 
                 </ion-card-content>
-            </IonCard>
+            </div>
 
             <!-- recomended -->
-            <IonCard class="bg-transparent">
-                <IonList class="flex flex-row flex-nowrap">
-                    <IonItem>
-                        <IonAvatar class="w-20 h-20">
+            <div class="bg-transparent mt-8">
+                <IonCardHeader>
+                    <IonCardTitle class="text-lg">Recommended for you</IonCardTitle>
+                </IonCardHeader>
+                <div class="scroll flex flex-row flex-nowrap gap-4 overflow-x-auto overflow-y-hidden no-scrollbar text-center mx-4" :scrollable="true">
+                    <div v-for="i in 10" :key="i" class="flex flex-col gap-2">
+                        <IonAvatar class="w-24 h-auto">
                             <img alt="Silhouette of a person's head"
                                 src="https://ionicframework.com/docs/img/demos/avatar.svg" />
                         </IonAvatar>
-                    </IonItem>
-
-                    <IonItem>
-                        <IonAvatar class="w-20 h-auto">
-                            <img alt="Silhouette of a person's head"
-                                src="https://ionicframework.com/docs/img/demos/avatar.svg" />
-                        </IonAvatar>
-                    </IonItem>
-
-                    <IonItem>
-                        <IonAvatar class="w-20 h-auto">
-                            <img alt="Silhouette of a person's head"
-                                src="https://ionicframework.com/docs/img/demos/avatar.svg" />
-                        </IonAvatar>
-                    </IonItem>
-
-                    <!-- <IonItem>
-                        <IonAvatar class="w-20 h-auto">
-                            <img alt="Silhouette of a person's head"
-                                src="https://ionicframework.com/docs/img/demos/avatar.svg" />
-                        </IonAvatar>
-                    </IonItem>
-
-                    <IonItem>
-                        <IonAvatar class="w-20 h-auto">
-                            <img alt="Silhouette of a person's head"
-                                src="https://ionicframework.com/docs/img/demos/avatar.svg" />
-                        </IonAvatar>
-                    </IonItem> -->
-                </IonList>
-            </IonCard>
+                        <span class="font-bold">MHD</span>
+                    </div>
+                </div>
+            </div>
         </IonContent>
     </IonPage>
 </template>
+
+<style scoped>
+/* Hide scrollbar for Chrome, Safari and Opera */
+.no-scrollbar::-webkit-scrollbar {
+    display: none;
+}
+
+/* Hide scrollbar for IE, Edge and Firefox */
+.no-scrollbar {
+    -ms-overflow-style: none;  /* IE and Edge */
+    scrollbar-width: none;  /* Firefox */
+}
+
+</style>
 
 <script setup>
 import { IonPage, IonContent, IonAvatar, IonCard, IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonList, IonItem } from '@ionic/vue';
