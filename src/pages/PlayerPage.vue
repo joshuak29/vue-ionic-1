7de6068b8314e15f806cce7ac62266829de6c146
joshuak29 <rockjoshk29@gmail.com>
2024-05-28 16:14:34 +0200

    player like and share

--- src/pages/PlayerPage.vue
+++ src/pages/PlayerPage.vue
@@ -14,13 +14,13 @@
             </div>
 
             <!-- Actions -->
-            <div class="flex flex-row justify-between items-center px-8">
-                <ArrowsRightLeftIcon class=" h-6" />
+            <div class="flex flex-row justify-between items-center px-12">
+                <HeartIcon class=" h-6" />
                 <div class="flex flex-col justify-center items-center">
                     <span class="font-bold text-xl">J'me Tire</span>
                     <span>Gims</span>
                 </div>
-                <ArrowPathRoundedSquareIcon class="h-6" />
+                <ShareIcon class=" h-6" />
             </div>
 
             <!-- slider -->
@@ -29,7 +29,8 @@
             </div>
 
             <!-- Press Play -->
-            <div class="flex flex-row justify-center py-2 px-6 text-xl font-bold items-center gap-1">
+            <div class="flex flex-row justify-between items-center gap-1 py-2 px-6 mb-12 text-xl font-bold">
+                <ArrowsRightLeftIcon class=" h-6 pr-12" />
                 <BackwardIcon class=" h-10" />
                 <div @click="pause = !pause">
                     <PauseCircleIcon class="h-20" v-if="pause" />
@@ -37,7 +38,15 @@
                 </div>
 
                 <ForwardIcon class=" h-10" />
+                <ArrowPathRoundedSquareIcon class="h-6 pl-12" />
             </div>
+
+            <!-- details -->
+            <!-- <div class="flex flex-col text-sm mb-8 mx-4">
+                <div><span class="font-semibold text-lg">Released:</span> 14 May 2020</div>
+                <div><span class="font-semibold text-lg">Artists:</span> Maitre Gims, Ya Levis, Koba Lad, MHD</div>
+                <div><span class="font-semibold text-lg">Plays:</span> 23099</div>
+            </div> -->
         </IonContent>
 
 
@@ -46,7 +55,7 @@
 
 <script setup>
 import { IonPage, IonHeader, IonContent, IonRange } from '@ionic/vue';
-import { ChevronDownIcon, ListBulletIcon, PlayCircleIcon, PauseCircleIcon, ForwardIcon, BackwardIcon, ArrowsRightLeftIcon, ArrowPathRoundedSquareIcon } from '@heroicons/vue/24/solid';
+import { ChevronDownIcon, ListBulletIcon, PlayCircleIcon, PauseCircleIcon, ForwardIcon, BackwardIcon, ArrowsRightLeftIcon, ArrowPathRoundedSquareIcon, HeartIcon, ShareIcon } from '@heroicons/vue/24/solid';
 import { ref } from "vue";
 import { useRouter } from 'vue-router';
 
